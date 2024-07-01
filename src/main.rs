e6cd5ea76add98e8d51884e454333f706e50a6a1
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-07-01 14:38:10 +0200

    added listener and handling of connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::prelude::Read;
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    // reading the content of the stream into the bytes buffer
+    stream.read(&mut buffer).unwrap();
+
+    //TODO: do something with the content of the request
+
+    // returning a default response
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
